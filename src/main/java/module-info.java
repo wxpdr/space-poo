261bdbb3daa0d8b9e144dbb4e80d41c9d9123297
Wendy Pedrosa <105825286+wxpdr@users.noreply.github.com>
2025-04-28 15:59:44 -0300

    Upload new src

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module br.com.joaocarloslima {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
+
 
     opens br.com.joaocarloslima to javafx.fxml;
     exports br.com.joaocarloslima;
