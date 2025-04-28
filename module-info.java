bdc4bfb08e5e0d372c8e6247a76d9e51b6a107ab
Wendy Pedrosa <105825286+wxpdr@users.noreply.github.com>
2025-04-28 16:02:36 -0300

    Delete module-info.java

--- module-info.java
+++
@@ -1,9 +0,0 @@
-module br.com.joaocarloslima {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
-
-
-    opens br.com.joaocarloslima to javafx.fxml;
-    exports br.com.joaocarloslima;
-}
